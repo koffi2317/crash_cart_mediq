06be02a58259dcd73c0cb7285a7768326955ad42
koffi2317 <koffiakakpo23@gmail.com>
2026-03-14 10:23:11 -0400

    pseudo code

---
+++ crash_cart_mediq/lib/detector.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+class Detector extends StatelessWidget {
+  const Detector({super.key});
+
+
+int Fr;
+int Sat;
+String medicament;
+
+void analyser(row){ 
+
+
+
+
+void _isWrongDrug(row);
+
+_void isWrongDose(row);
+
+_void isWrongAdministration(row);
+
+_void isIllogicalForVitals(row);
+
+
+
+}
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
